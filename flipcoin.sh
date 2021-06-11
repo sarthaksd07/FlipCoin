d70b0a8cc5b1b4c2cadf4b10f20ccf8e9038daa5
sarthaksd07 <sarthaksd07@gmail.com>
2021-06-11 15:15:02 +0530

    Added:Difference of min 2 points

--- flipcoin.sh
+++ flipcoin.sh
@@ -1,7 +1,22 @@
 #!/bin/bash -x
 head_count=1
 tail_count=1
-while [ $head_count -le 21 -o $tail_count -le 21 ]
+while [ $head_count -le 21 -a $tail_count -le 21 ]
+do
+	result=$((RANDOM%2));
+	if [ $result -eq 0 ];
+	then
+		echo "Head is the winner";
+		head_count=$(($head_count+1));
+	elif [ $result -eq 1 ];
+	then
+		echo "Tail is the winner";
+		tail_count=$(($tail_count+1));
+	fi
+done
+
+
+while [ $(($head_count-$tail_count)) -ge 2 ]
 do
 	result=$((RANDOM%2));
 	if [ $result -eq 0 ];
