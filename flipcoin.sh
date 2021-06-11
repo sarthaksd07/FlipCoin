26fc437a99804837d9b4c3b264d4d3e518ebaf10
sarthaksd07 <sarthaksd07@gmail.com>
2021-06-11 15:13:31 +0530

    Added:Win or tie

--- flipcoin.sh
+++ flipcoin.sh
@@ -1,14 +1,16 @@
 #!/bin/bash -x
-count=1
-while [ $count -le 10 ]
+head_count=1
+tail_count=1
+while [ $head_count -le 21 -o $tail_count -le 21 ]
 do
 	result=$((RANDOM%2));
 	if [ $result -eq 0 ];
 	then
 		echo "Head is the winner";
+		head_count=$(($head_count+1));
 	elif [ $result -eq 1 ];
 	then
 		echo "Tail is the winner";
+		tail_count=$(($tail_count+1));
 	fi
-count=$(($count+1));
 done
